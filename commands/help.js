0202a82644cba9fc4f95c7177baa562fcff9cf3d
unknown <aaron.pagoada@gmail.com>
2022-04-24 23:52:13 -0400

    Indentations

--- commands/help.js
+++ commands/help.js
@@ -33,6 +33,6 @@ module.exports = {
 
     data.push(`Cooldown: ${command.cooldown || 3} second(s)`);
 
-	  message.channel.send(data, {split: true});
+    message.channel.send(data, {split: true});
   }
 };
